20c039740a4cde76e31a4491a01a34c354ac1d84
DevBasem <engr.basemahmed@gmail.com>
2024-01-30 10:29:31 -0800

    task 1 to 5

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -31,5 +32,10 @@ typedef struct binary_tree_s heap_t;
 /* Function prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif /* BINARY_TREES_H */
